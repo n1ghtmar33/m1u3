429166e2521ca14c594a0b62dc7baeb7fbb092ab
eric <erik.ivanov1337@gmail.com>
2024-03-24 18:10:00 +0200

    new string

--- bot.py
+++ bot.py
@@ -24,3 +24,5 @@ def ban_user(message):
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
 bot.infinity_polling(none_stop=True)
+
+#12333232123
